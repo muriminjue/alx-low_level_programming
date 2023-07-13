4b3c594257ee2037cbf21865e91489ed5684318c
muriminjue <muriminjue.nm@gmail.com>
2023-07-13 11:32:21 -0700

    task 4-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - app entry
  * uses puts to print string
- * returns 0
+ *
+ * Return: 0 (successful)
  */
-int main()
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
