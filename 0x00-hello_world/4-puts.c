e6ede8a403ee077dae2f75346305a66d2b6e0183
muriminjue <muriminjue.nm@gmail.com>
2023-07-13 11:35:39 -0700

    task 4-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - app entry
  * uses puts to print string
- * 
+ *
  * Return: 0(successful)
  */
-int main()
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
