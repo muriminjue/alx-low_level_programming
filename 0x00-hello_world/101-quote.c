f726b5e94c605574cdd41e254cb4752aaef865c1
muriminjue <muriminjue.nm@gmail.com>
2023-07-13 12:44:31 -0700

    optional

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * main - App entry
@@ -9,7 +10,7 @@
  */
 int main(void)
 {
-char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDOUT_FILENO, quote, strlen(quote));
 return(1);
 }
