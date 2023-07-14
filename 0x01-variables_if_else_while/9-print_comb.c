2afcfb71a4fe01c6f6b93be673f50c687bc8d15a
muriminjue <muriminjue.nm@gmail.com>
2023-07-14 02:09:02 -0700

    task 9 fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,17 @@
  * Return: 0 (success)
  */
 int main(void) {
-    int num;
-    
-    for (num = 0; num < 10; num++)
-    {
-       putchar(48 + num);
-       if(num != 9){
-           putchar(',');
-           putchar(' ');
-       }
-    }
-    putchar('\n');
-    return (0);
+int num;
+
+for (num = 0; num < 10; num++)
+{
+  putchar(48 + num);
+  if(num != 9)
+  {
+    putchar(',');
+    putchar(' ');
+  }
+}
+putchar('\n');
+return (0);
 }
