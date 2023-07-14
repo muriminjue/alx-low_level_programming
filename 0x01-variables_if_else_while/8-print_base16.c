0e5e6ab5d53f36be771e75b86d6130720bbe3a16
muriminjue <muriminjue.nm@gmail.com>
2023-07-14 02:18:21 -0700

    task 8-9 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,20 +6,21 @@
  * 
  * Return: 0 (success)
  */
-int main(void) {
-    int hex;
+int main(void)
+{
+int hex;
 
-    for (hex = 0; hex < 16; hex++)
-    {
-        if(hex <10)
-        {
-            putchar('0' + hex);
-        }
-        else
-        {
-            putchar('a' + (hex-10));
-        }
-    }
-    putchar('\n');
-    return (0);
+for (hex = 0; hex < 16; hex++)
+{
+if (hex <10)
+{
+putchar('0' + hex);
+}
+else
+{
+putchar('a' + (hex-10));
+}
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
  * 
  * Return: 0 (success)
  */
-int main(void) {
+int main(void)
+{
 int num;
 
 for (num = 0; num < 10; num++)
 {
-  putchar(48 + num);
-  if(num != 9)
-  {
-    putchar(',');
-    putchar(' ');
-  }
+putchar(48 + num);
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
