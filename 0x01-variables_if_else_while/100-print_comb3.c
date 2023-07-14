d1e3368e0b1bf043b538e24ae114bfa29c986b55
muriminjue <muriminjue.nm@gmail.com>
2023-07-14 04:21:52 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - app entry
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+int num;
+int num1;
+
+for (num = 0; num <= 9; num++)
+{
+for (num1 = num + 1 ; num1 <= 9; num1++)
+{
+putchar(48 + num);
+putchar(48 + num1);
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
