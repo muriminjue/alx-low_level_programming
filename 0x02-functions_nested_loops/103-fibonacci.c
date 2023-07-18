2bce34dbfe3e1b6738f03496b2d2dae1c0924dd5
muriminjue <muriminjue.nm@gmail.com>
2023-07-18 14:54:04 -0700

    fib1

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - first 50 fibbonachi
+ * Return: 0 success
+ */
+int main(void) 
+{
+long int fib0 = 1, fib1 =2, prevfeb, sum = 2;
+
+while (fib1 <= 4000000)
+{
+prevfeb = fib1;
+fib1 = fib0 + fib1;
+if (fib1 % 2 == 0)
+{
+sum = sum + fib1;   
+}
+fib0 = prevfeb;
+}
+printf("%ld\n",sum);
+return(0);
+}
