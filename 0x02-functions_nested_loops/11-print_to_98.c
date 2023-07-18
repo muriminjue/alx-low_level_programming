fb6c2afe2c6897df771282e0141a2d89ee6b30d8
muriminjue <muriminjue.nm@gmail.com>
2023-07-18 11:51:01 -0700

    last mandatory

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - print from n to 98
+ * @n: starting pint integer
+ */
+
+void print_to_98(int n)
+{
+int count = 98 - n;
+int m;
+
+if (count < 0)
+{
+count = -count;
+}
+for (m = 0; m < count; m++)
+{
+if (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+else
+{
+printf("%d, ", n);
+n--;
+}
+}
+printf("98");
+}
