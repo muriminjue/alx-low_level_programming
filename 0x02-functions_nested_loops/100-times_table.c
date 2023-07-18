cc9c01f3d984553dd8963ce2da12a104e2294d29
muriminjue <muriminjue.nm@gmail.com>
2023-07-18 12:29:24 -0700

    more tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+/**
+ * times_table - table of multiples
+ * @n: integer to be computed
+ */
+
+void print_times_table(int n)
+{
+if (n < 15 && n > 0)
+{
+int j;
+
+for (j = 0; j <= 9; j++)
+{
+int m;
+
+for (m = 0; m <= 9; m++)
+{
+int value = j * m;
+
+if (m > 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+if (value / 10 < 1 && m > 0)
+{
+_putchar(' ');
+}
+if (value > 9)
+{
+_putchar('0' + value / 10);
+_putchar('0' + value % 10);
+}
+else
+{
+_putchar('0' + value);
+}
+
+}
+_putchar('\n');
+}
+}
+}
