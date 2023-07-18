5311879a5817f36af6ff9f2724319612b86f5285
muriminjue <muriminjue.nm@gmail.com>
2023-07-18 14:45:42 -0700

    fib0

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,12 +4,12 @@
  * main - first 50 fibbonachi
  * Return: 0 success
  */
-int main(void) 
+int main(void)
 {
 int count;
 long int fib0 = 1, fib1 = 2, prevfeb;
-printf("%ld, %ld", fib0, fib1);
 
+printf("%ld, %ld", fib0, fib1);
 for (count = 0; count < 48; count++)
 {
 prevfeb = fib1;
@@ -18,5 +18,5 @@ printf(", %ld", fib1);
 fib0 = prevfeb;
 }
 printf("\n");
-return(0);
+return (0);
 }
