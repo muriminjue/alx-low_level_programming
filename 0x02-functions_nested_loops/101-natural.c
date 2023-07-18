bbdfe27bafcc3a3e24c8f1ad86b22974f71d7f37
muriminjue <muriminjue.nm@gmail.com>
2023-07-18 12:19:33 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,9 +3,10 @@
 /**
  * main - print the sum of all the multiples of 3 or 5 below 1024.
  *
- * Return: sum
+ * Return: 0 success
  */
-int main(void) {
+int main(void)
+{
 int n, sum = 0;
 
 for (n = 0; n < 1024; n++)
@@ -16,5 +17,5 @@ sum += n;
 }
 }
 printf("%d\n", sum);
-return (sum);
+return (0);
 }
