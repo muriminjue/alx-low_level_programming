9efede638a4fe3200e638621a9a0998948307e89
muriminjue <muriminjue.nm@gmail.com>
2023-07-20 12:41:32 -0700

    tasks 0-2

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if uppercase
+ * @c: The character to check
+ * Return: On success uppercase 1 else 0
+ */
+int _isupper(int c)
+{
+if(c >= 'a' && c <= 'z' )
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+/**
+ * file: main.h
+ * author: Mathew M.
+ * description: Prototypes of all your functions
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
