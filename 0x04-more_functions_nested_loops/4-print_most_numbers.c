222111fa5592a77cb372afcef886f9e6264a8d1e
muriminjue <muriminjue.nm@gmail.com>
2023-07-20 13:53:06 -0700

    tasks 3-6

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,14 @@
  */
 void print_most_numbers(void)
 {
-int num = 0;
+int num;
 
-while (num <= 9)
+for (num = 0; num <= 9; num++)
 {
 if (num != 2 && num != 4)
 {
 _putchar('0' + num);
 }
-num++;
 }
 _putchar('\n');
 }
