92e0df6563c5341140263f1179dc092856154c5d
muriminjue <muriminjue.nm@gmail.com>
2023-07-24 11:37:12 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#includes "main.h"
+
+/**
+ * _puts - prints string
+ * @str: string
+ *
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (i >= 0)
+{
+if (s[i] == '\0')
+{
+break;
+}
+_putchar(s[i]);
+i++;
+}
+}
