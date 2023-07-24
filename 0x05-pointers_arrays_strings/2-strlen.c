754791b2ee34729efe62037df0ef7f6be634bd5d
muriminjue <muriminjue.nm@gmail.com>
2023-07-24 10:57:44 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - gewts length of a string
+ * @s: string
+ * Return: length of the string as integer
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (i >= 0)
+{
+_putchar(s[i]);
+if (s[i] == '\0')
+{
+break;
+}
+i++;
+}
+
+return (i);
+}
