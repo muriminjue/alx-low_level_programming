70a09462e2a4fa0e918275bdd6e3cae2a7a41a7a
muriminjue <muriminjue.nm@gmail.com>
2023-07-27 15:31:29 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: param pointer to a string of char
+ * Return: string str of chars encoded
+ */
+
+char *leet(char *str)
+{
+int count = 0, m;
+int lower_letters[] = {97, 101, 111, 116, 108};
+int upper_letters[] = {65, 69, 79, 84, 76};
+int rep_umbers[] = {52, 51, 48, 55, 49};
+
+while (str[count] != '\0')
+{
+for (m = 0; m < 5; m++)
+{
+if (str[count] == lower_letters[m] || str[count] == upper_letters[m])
+{
+str[count] = numbers[m];
+break;
+}
+}
+count++;
+}
+return (str);
+}
