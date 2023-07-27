cc6cf36689b7db0ad2ea6843ccee81e789a396a3
muriminjue <muriminjue.nm@gmail.com>
2023-07-27 14:25:58 -0700

    task 4-5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
-
+void reverse_array(int *a, int n);
 
 #endif /* MAIN_H */
