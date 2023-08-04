04e8d7962878e196a5955ff5b1f62c24fecd2bbb
muriminjue <muriminjue.nm@gmail.com>
2023-08-04 07:19:15 -0700

    task 0-1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: array of argument pointers
  * Return: 0 id succes
  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc - 1);
 return (0);
