e5fbfa00c309176f5f8fc0a434b46d0b31bf1046
muriminjue <muriminjue.nm@gmail.com>
2023-08-04 08:39:14 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 /**
- * main - sum args
+ * main - sum of args
  * @argc: number of arguments
  * @argv: array of argument pointers
  * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-int sum = 0, count = argc - 1;
+int sum = 0, count = argc - 1, pos;
 
 while (count > 0)
 {
-if (!isdigit(*argv[count]))
+for (pos = 0; pos < strlen(argv[count]); pos++)
+{
+if (!isdigit(argv[count][pos]))
 {
 printf("Error\n");
 return (1);
+}
 }
 sum = sum + atoi(argv[count]);
 count--;
