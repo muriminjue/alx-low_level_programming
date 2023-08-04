7767afb8b692fadd6f0cca29d31022540b46c22c
muriminjue <muriminjue.nm@gmail.com>
2023-08-04 06:19:30 -0700

    task 0-1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printsfiles name
+ * @argc: number of arguments
+ * @argv: array of argument pointers
+ * Return: 0 id succes
+ */
+int main(int argc, char *argv[]) 
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  prints count of args
+ * @argc: number of arguments
+ * @argv: array of argument pointers
+ * Return: 0 id succes
+ */
+int main(int argc, char *argv[]) 
+{
+printf("%d\n", argc - 1);
+return (0);
+}
