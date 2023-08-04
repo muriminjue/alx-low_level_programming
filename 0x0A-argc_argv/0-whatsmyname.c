364a28834417ad3c886e1db02f8549391d0ad066
muriminjue <muriminjue.nm@gmail.com>
2023-08-04 07:14:40 -0700

    task 0-1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of argument pointers
  * Return: 0 id succes
  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[] __attribute__((unused))) 
 {
 printf("%s\n", argv[0]);
 return (0);
